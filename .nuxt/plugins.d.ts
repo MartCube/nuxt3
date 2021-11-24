2dc999b8727a8d38a7055fddcabe0d4669abfa78
martcube <martingjorceski@gmail.com>
2021-11-24 18:18:59 +0100

    sanity

--- .nuxt/plugins.d.ts
+++ .nuxt/plugins.d.ts
@@ -6,10 +6,10 @@ type Decorate<T extends Record<string, any>> = { [K in keyof T as K extends stri
 type InjectionType<A extends Plugin> = A extends Plugin<infer T> ? Decorate<T> : unknown
 
 type NuxtAppInjections = 
+  InjectionType<typeof import('sanity/plugin').default> &
   InjectionType<typeof import('../node_modules/nuxt3/dist/meta/runtime/lib/vueuse-head.plugin').default> &
   InjectionType<typeof import('../node_modules/nuxt3/dist/meta/runtime/plugin').default> &
   InjectionType<typeof import('../node_modules/nuxt3/dist/pages/runtime/router').default> &
-  InjectionType<typeof import('sanity/plugin').default> &
   InjectionType<typeof import('../node_modules/@nuxt/nitro/dist/runtime/app/nitro.client').default> &
   InjectionType<typeof import('../#build/components').default>
 
